e3d160374120e741c849633d643ee70bc1143356
shivangjhalani <shivang2004jhalani@gmail.com>
2025-10-27 18:54:36 +0530

    neotree popup

--- config/plugins/editor/neo-tree.nix
+++ config/plugins/editor/neo-tree.nix
@@ -9,6 +9,24 @@
     ];
     addBlankLineAtTop = false;
 
+    settings = {
+      enable_diagnostics = true;
+      enable_git_status = true;
+      enable_modified_markers = true;
+      enable_refresh_on_write = true;
+      close_if_last_window = true;
+      popup_border_style = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
+      window = {
+        position = "float";
+        width = 40;
+        height = 15;
+        auto_expand_width = false;
+        # mappings = {
+        #   "<space>" = "none";
+        # };
+      };
+    };
+
     filesystem = {
       bindToCwd = false;
       followCurrentFile = {
